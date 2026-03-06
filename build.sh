e2f57a38a8432062c67443842134c69c7e067f2f
Artur Zacniewski <a.zacniewski@gmail.com>
2026-03-06 11:06:01 +0100

    Adding creation of superuser in build script

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth.hashers import make_password;from django.contrib.auth.models import User; admin = User(username='username', email='user@example.com', password=make_password('password'), is_superuser=True,is_staff=True);admin.save()"
